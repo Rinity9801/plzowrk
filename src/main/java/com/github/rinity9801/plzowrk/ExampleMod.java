173a7d881557da347a788caa68d6b2b451a46e44
GitHub Action <action@github.com>
2024-01-28 07:01:38 +0000

    Initialize template repository proper
    Set package name to com.github.rinity9801.plzowrk
    Set project name to plzowrk
    Set modid to plzowrk

--- src/main/java/com/github/rinity9801/plzowrk/ExampleMod.java
+++ src/main/java/com/github/rinity9801/plzowrk/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.rinity9801.plzowrk;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "plzowrk", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/rinity9801/plzowrk/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/rinity9801/plzowrk/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.rinity9801.plzowrk.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
